ce1e8d5b483990f55cbe2fb08c599f201934fe6f
Michael Smith <msmith@dev-10.com>
2022-06-10 15:11:49 -0400

    need to commit to pull

--- capstone-backend/src/main/java/learn/capstone/data/UserJdbcTemplateRepository.java
+++ capstone-backend/src/main/java/learn/capstone/data/UserJdbcTemplateRepository.java
@@ -1,11 +1,18 @@
 package learn.capstone.data;
 
+import learn.capstone.data.mappers.AuditionMapper;
+import learn.capstone.data.mappers.UserMapper;
 import learn.capstone.models.User;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
 
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 import java.util.List;
+import java.util.Locale;
 
 @Repository
 public class UserJdbcTemplateRepository implements UserRepository{
@@ -16,26 +23,55 @@ public class UserJdbcTemplateRepository implements UserRepository{
 
     @Override
     public List<User> findAll() {
-        return null;
+        final String sql = "select * from user;";
+
+        return jdbcTemplate.query(sql, new UserMapper());
     }
 
     @Override
-    public User findById() {
-        return null;
+    public User findById(int userId) {
+        final String sql = "select *" +
+                "from user where user_id = ?;";
+
+        return jdbcTemplate.query(sql, new UserMapper(), userId)
+                .stream().findFirst().orElse(null);
     }
 
     @Override
     public User add(User user) {
-        return null;
+        final String sql = "insert into user (access_type, first_name, last_name) " +
+                "values (?, ?, ?);";
+
+        KeyHolder keyholder = new GeneratedKeyHolder();
+        int rowsAffected = jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, user.getAccessType().toString().toLowerCase(Locale.ROOT));
+            ps.setString(2, user.getFirstName());
+            ps.setString(3, user.getLastName());
+            return ps;
+        }, keyholder);
+
+        if (rowsAffected <= 0) {
+            return null;
+        }
+
+        user.setUserId(keyholder.getKey().intValue());
+        return user;
     }
 
     @Override
-    public User update(User user) {
-        return null;
+    public boolean update(User user) {
+        final String sql = "update user set " +
+                "user_id = ?, access_type = ?, first_name = ?, last_name = ? " +
+                "where user_id = ?;";
+
+        return jdbcTemplate.update(sql,
+                user.getUserId(), user.getAccessType(), user.getFirstName(), user.getLastName(),
+                user.getUserId()) > 0;
     }
 
     @Override
     public boolean deleteById(int id) {
-        return false;
+        return jdbcTemplate.update("delete from user where user_id = ?", id) > 0;
     }
 }
--- capstone-backend/src/test/java/learn/capstone/TestHelper.java
+++ capstone-backend/src/test/java/learn/capstone/TestHelper.java
@@ -1,12 +1,13 @@
 package learn.capstone;
 
+import learn.capstone.models.AccessType;
 import learn.capstone.models.Audition;
+import learn.capstone.models.User;
 
 public class TestHelper {
 
-    public static Audition makeAudition3() {
-        Audition audition = new Audition();
-        audition.setAuditionId(3);
-        audition.set
+    public User makeUser(){
+        User newUser = new User(5, "Lin-Manuel", "Miranda", AccessType.AUDITIONEE);
+        return newUser;
     }
 }
