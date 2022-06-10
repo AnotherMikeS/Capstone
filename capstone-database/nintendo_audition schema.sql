1a44d19115f7cd6a87baf2364b3832d0b73553b6
Shelley Nixon <shellnix4@gmail.com>
2022-06-10 10:26:17 -0400

    Updated SQL diagram

--- capstone-database/nintendo_audition schema.sql
+++ capstone-database/nintendo_audition schema.sql
@@ -9,28 +9,38 @@ create table `user` (
     last_name char(50)
 );
 
-create table auditionee (
-	auditionee_id int primary key auto_increment,
-    user_id int,
-    part_id int,
-    `date` date,
-    selection char(100)
-    );
-    
-create table manager (
-	manager_id int primary key auto_increment,
-    user_id int
-);
-
 create table part (
 	part_id int primary key auto_increment,
     `role` enum('singing', 'acting')
 );
 
+create table auditionee (
+	auditionee_id int primary key auto_increment,
+    user_id int not null,
+    part_id int not null,
+    `date` date,
+    selection char(100),
+     foreign key(user_id)
+    references `user`(user_id),
+     foreign key(part_id)
+    references part(part_id)
+    );
+    
+create table manager (
+	manager_id int primary key auto_increment,
+    user_id int,
+     foreign key(user_id)
+    references `user`(user_id)
+);
+
 create table auditions (
 	audition_id int primary key auto_increment,
-    auditiontee_id int,
-    part_id int
+    auditionee_id int,
+    part_id int,
+     foreign key(auditionee_id)
+    references auditionee(auditionee_id),
+     foreign key(part_id)
+    references part(part_id)
 );
     
     
