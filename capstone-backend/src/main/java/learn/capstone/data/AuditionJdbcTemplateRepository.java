67f0c4a4f35a9ab63a7b62922d683c588ca4a1c7
Gregory Gennaro <gennarogreg@gmail.com>
2022-06-10 11:22:24 -0500

    IDK

---
+++ capstone-backend/src/main/java/learn/capstone/data/AuditionJdbcTemplateRepository.java
@@ -0,0 +1,72 @@
+package learn.capstone.data;
+
+import learn.capstone.data.mappers.AuditionMapper;
+import learn.capstone.models.Audition;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
+
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.util.List;
+
+@Repository
+public class AuditionJdbcTemplateRepository implements AuditionRepository{
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public AuditionJdbcTemplateRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public List<Audition> findAll() {
+        final String sql = "select audition_id, auditionee_id, part_id from auditions;";
+
+        return jdbcTemplate.query(sql, new AuditionMapper());
+    }
+
+    @Override
+    public Audition findById(int auditionId) {
+        final String sql = "select audition_id, auditionee_id, part_id" +
+                "from auditions where audition_id = ?;";
+
+        return jdbcTemplate.query(sql, new AuditionMapper(), auditionId)
+                .stream().findFirst().orElse(null);
+    }
+
+    @Override
+    public Audition add(Audition audition) {
+        final String sql = "insert into auditions (auditionee_id), (part_id)" +
+                "values (?);";
+
+        KeyHolder keyholder = new GeneratedKeyHolder();
+        int rowsAffected = jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setInt(1, audition.getAuditioneeId());
+            return ps;
+        }, keyholder);
+
+        if (rowsAffected <= 0) {
+            return null;
+        }
+
+        audition.setAuditionId(keyholder.getKey().intValue());
+        return audition;
+    }
+
+    @Override
+    public boolean update(Audition audition) {
+        final String sql = "update auditions set" +
+                "auditionee_id = ?, part_id = ? where audition_id = ?;";
+
+        return jdbcTemplate.update(sql, audition.getAuditioneeId(), audition.getPartId(),
+                audition.getAuditionId()) > 0;
+    }
+
+    @Override
+    public boolean deleteById(int auditionId) {
+        return jdbcTemplate.update("delete from auditions where audition_id = ?", auditionId) > 0;
+    }
+}
