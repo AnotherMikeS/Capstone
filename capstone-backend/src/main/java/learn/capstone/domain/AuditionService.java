6bb7f07d2867328a339b70eec3e838d571c18389
Gregory Gennaro <gennarogreg@gmail.com>
2022-06-10 15:16:51 -0500

    Result and ResultType

--- capstone-backend/src/main/java/learn/capstone/domain/AuditionService.java
+++ capstone-backend/src/main/java/learn/capstone/domain/AuditionService.java
@@ -1,4 +1,88 @@
 package learn.capstone.domain;
 
+import learn.capstone.data.AuditionRepository;
+import learn.capstone.models.Audition;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class AuditionService {
+
+    private final AuditionRepository repository;
+
+    public AuditionService(AuditionRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Audition> findAll() {
+        return repository.findAll();
+    }
+
+    public Audition findById(int auditionId) {
+        return repository.findById(auditionId);
+    }
+
+    public Result<Audition> add(Audition audition) {
+        Result<Audition> result = validate(audition);
+        if (!result.isSuccess()) {
+            return result;
+        }
+
+        if (audition.getAuditionId() !=0) {
+            result.addMessage("Audition ID cannot be set for 'add' operation.", ResultType.INVALID);
+            return result;
+        }
+
+        audition = repository.add(audition);
+        result.setPayload(audition);
+        return result;
+    }
+
+    public Result<Audition> update(Audition audition) {
+        Result<Audition> result = validate(audition);
+        if (!result.isSuccess()) {
+            return result;
+        }
+
+        if (audition.getAuditionId() <= 0) {
+            result.addMessage("Audition ID must be set for 'update' operation.", ResultType.INVALID);
+            return result;
+        }
+
+        if (!repository.update(audition)) {
+            String msg = String.format("Audition ID: %s was not found.", audition.getAuditionId());
+            result.addMessage(msg, ResultType.NOT_FOUND);
+        }
+        return result;
+    }
+
+    public Result<Audition> deleteById(int auditionId) {
+        Result<Audition> result = new Result<>();
+        boolean success = repository.deleteById(auditionId);
+        if (!success) {
+            result.addMessage("Unable to delete audition.", ResultType.INVALID);
+            return result;
+        }
+
+        return result;
+    }
+
+    private Result<Audition> validate(Audition audition) {
+        Result<Audition> result = new Result<>();
+
+        if (audition == null) {
+            result.addMessage("Auditions cannot be null.", ResultType.INVALID);
+            return result;
+        }
+
+        List<Audition> all = repository.findAll();
+        for (Audition a : all) {
+            if (a.getAuditioneeId() == audition.getAuditioneeId() && a.getPartId() == audition.getPartId()) {
+                result.addMessage("Duplicate Auditions are not allowed.", ResultType.INVALID);
+            }
+        }
+
+        return result;
+    }
 }
--- capstone-backend/src/main/java/learn/capstone/domain/Result.java
+++ capstone-backend/src/main/java/learn/capstone/domain/Result.java
@@ -1,24 +1,49 @@
 package learn.capstone.domain;
 
-import learn.capstone.models.Auditionee;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
-public class Result {
+public class Result<T> {
+    private final ArrayList<String> messages = new ArrayList<>();
+    private ResultType type = ResultType.SUCCESS;
+    private T payload;
+
+    public ResultType getType() {
+        return type;
+    }
+
     public boolean isSuccess() {
-        return false;
+        return type == ResultType.SUCCESS;
     }
 
-    public void setPayload(Auditionee withId) {
+    public T getPayload() {
+        return payload;
     }
 
-//    - private final ArrayList<String> messages
-//        - private ResultType type
-//        - private T payload;
-//        - public ResultType getType()
-//        - public boolean isSuccess()
-//        - public T getPayload()
-//        - public void setPayload()
-//        - public List<String> getMessages()
-//        - public void addMessages()
-//        - Overrides
+    public void setPayload(T payload) {
+        this.payload = payload;
+    }
 
+    public List<String> getMessages() {
+        return new ArrayList<>(messages);
+    }
+
+    public void addMessage(String message, ResultType type) {
+        messages.add(message);
+        this.type = type;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Result<?> result = (Result<?>) o;
+        return Objects.equals(messages, result.messages) && type == result.type && Objects.equals(payload, result.payload);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(messages, type, payload);
+    }
 }
