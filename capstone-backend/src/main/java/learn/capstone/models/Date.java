937036485169189be597617e5cf6c94f9d86a254
Gregory Gennaro <gennarogreg@gmail.com>
2022-06-10 14:43:37 -0500

    Enum solved

--- capstone-backend/src/main/java/learn/capstone/models/Date.java
+++ capstone-backend/src/main/java/learn/capstone/models/Date.java
@@ -1,7 +1,20 @@
 package learn.capstone.models;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public enum Date {
-    DATE_ONE,
-    DATE_TWO,
-    DATE_THREE
+    DATE_ONE(LocalDate.of(2022, 07, 01)),
+    DATE_TWO(LocalDate.of(2022, 07, 02)),
+    DATE_THREE(LocalDate.of(2022, 07, 03));
+
+    private LocalDate date;
+
+    private Date(LocalDate date) {
+        this.date = date;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
 }
