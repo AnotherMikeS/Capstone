ea9f1d0db5b94ee9f2187f3185fb3e5c2176626d
Gregory Gennaro <gennarogreg@gmail.com>
2022-06-15 10:17:35 -0500

    Test SQL

--- capstone-database/nintendo_audition_test.sql
+++ capstone-database/nintendo_audition_test.sql
@@ -4,8 +4,6 @@ use nintendo_audition_test;
 
 create table app_user (
 	app_user_id int primary key auto_increment,
-    first_name varchar(50),
-    last_name varchar(50),
     username varchar(100) not null unique,
     password_hash varchar(2048) not null,
     enabled boolean not null default(1)    
@@ -29,6 +27,15 @@ create table app_user_role (
          references app_role(app_role_id)
 );
 
+create table person (
+	person_id int primary key auto_increment,
+    app_user_id int not null,
+	first_name varchar(50) not null,
+    last_name varchar(50) not null,
+    foreign key (app_user_id)
+		references app_user(app_user_id)
+);
+
 create table part (
 	part_id int primary key auto_increment,
     `role` enum('singing', 'acting')
@@ -39,28 +46,26 @@ create table available_times (
     available_time varchar(50)
 );
     
-
 create table auditionee (
 	auditionee_id int primary key auto_increment,
     app_user_id int not null,
     part_id int not null,
     time_slot varchar(50),
     selection varchar(100),
-     foreign key(app_user_id)
-    references app_user(app_user_id),
-     foreign key(part_id)
-    references part(part_id)
+	foreign key(app_user_id)
+		references app_user(app_user_id),
+	foreign key(part_id)
+		references part(part_id)
     );
-    
 
 create table auditions (
 	audition_id int primary key auto_increment,
     auditionee_id int,
     part_id int,
-     foreign key(auditionee_id)
-    references auditionee(auditionee_id),
-     foreign key(part_id)
-    references part(part_id)
+	foreign key(auditionee_id)
+		references auditionee(auditionee_id),
+	foreign key(part_id)
+		references part(part_id)
 );
 
 delimiter //
@@ -78,6 +83,9 @@ begin
     
 	delete from part;
 	alter table part auto_increment = 1;
+    
+    delete from person;
+    alter table person auto_increment = 1;
 
 	delete from app_user_role;
     alter table app_user_role auto_increment = 1;
@@ -88,11 +96,12 @@ begin
 	delete from app_user;
     alter table app_user auto_increment = 1;
            
-    insert into app_user (app_user_id, username, password_hash, first_name, last_name) values
-		(1, 'sNixon', 'password', 'Shelley', 'Nixon'),
-		(2, 'mSmith', 'password', 'Michael', 'Smith'),
-        (3, 'gGennaro', 'password', 'Greg', 'Gennaro'),
-        (4, 'mMario', 'password', 'Mario', 'Mario');
+    insert into app_user (app_user_id, username, password_hash, enabled) values
+		(1, 'sNixon', 'password', 1),
+		(2, 'mSmith', 'password', 1),
+        (3, 'gGennaro', 'password', 1),
+        (4, 'mMario', 'password', 1),
+        (5, 'lMario', 'password', 1);
         
 	insert into app_role (app_role_id, access_type) values
 		(1, 'manager'),
@@ -103,6 +112,14 @@ begin
         (2, 2),
         (3, 1),
         (4, 2);
+        
+	insert into person (person_id, app_user_id, first_name, last_name) values
+		(1, 1, 'Shelley', 'Nixon'),
+        (2, 2, 'Mike', 'Smith'),
+        (3, 3, 'Greg', 'Gennaro'),
+        (4, 4, 'Mario', 'Mario'),
+        (5, 5, 'Luigi', 'Mario');
+        
     
     insert into part (part_id, `role`) values
         (1, 'singing'),
