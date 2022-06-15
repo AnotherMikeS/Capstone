461e1b2a46b7c758bcd3b5f02c01ab1880eb8469
Gregory Gennaro <gennarogreg@gmail.com>
2022-06-15 10:03:16 -0500

    updated sql

--- capstone-database/nintendo_audition schema.sql
+++ capstone-database/nintendo_audition schema.sql
@@ -4,8 +4,6 @@ use twelve_eyes_nintendo_audition;
 
 create table app_user (
 	app_user_id int primary key auto_increment,
-    first_name varchar(50),
-    last_name varchar(50),
     username varchar(100) not null unique,
     password_hash varchar(2048) not null,
     enabled boolean not null default(1)    
@@ -29,6 +27,15 @@ create table app_user_role (
          references app_role(app_role_id)
 );
 
+create table person (
+	person_id int primary key auto_increment,
+    app_user_id int not null,
+	first_name varchar(50) not null,
+    last_name varchar(50) not null,
+    foreign key (app_user_id)
+		references app_user(app_user_id)
+);
+
 create table part (
 	part_id int primary key auto_increment,
     `role` enum('singing', 'acting')
@@ -45,20 +52,20 @@ create table auditionee (
     part_id int not null,
     time_slot varchar(50),
     selection varchar(100),
-     foreign key(app_user_id)
-    references app_user(app_user_id),
-     foreign key(part_id)
-    references part(part_id)
+	foreign key(app_user_id)
+		references app_user(app_user_id),
+	foreign key(part_id)
+		references part(part_id)
     );
 
 create table auditions (
 	audition_id int primary key auto_increment,
     auditionee_id int,
     part_id int,
-     foreign key(auditionee_id)
-    references auditionee(auditionee_id),
-     foreign key(part_id)
-    references part(part_id)
+	foreign key(auditionee_id)
+		references auditionee(auditionee_id),
+	foreign key(part_id)
+		references part(part_id)
 );
     
     
