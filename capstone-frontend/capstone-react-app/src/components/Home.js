39dc05daed929792d351941a03d64fbd8494edeb
Shelley Nixon <shellnix4@gmail.com>
2022-06-16 12:49:40 -0400

    calloutBox working

--- capstone-frontend/capstone-react-app/src/components/Home.js
+++ capstone-frontend/capstone-react-app/src/components/Home.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 
 import images from "./images";
 import ImageSlider from "./imageSlider";
+import yoshi from "./yoshi-tpt.png";
 
 export default function Home() {
     return (
@@ -15,8 +16,9 @@ export default function Home() {
        */}
 
 <div className="calloutBox">
-  <div className="calloutBox-header">NINTENDO: the musical!</div>
-  <div className="calloutBox-container">auditioning now
+  <div className="calloutBox-header">NINTENDO: THE MUSICAL!</div>
+  <img className="yoshi" src={yoshi} style={{ padding: "0rem", width: 250, height: 150 }} />
+  <div className="calloutBox-container">Auditioning Now
   </div>
 </div>
       </main>
