0b4175cd6845dba7febc74f1e847f300211eee6a
Shelley Nixon <shellnix4@gmail.com>
2022-06-15 22:54:54 -0400

    refactored to use home.css instead of style.css

--- capstone-frontend/capstone-react-app/src/components/Home.js
+++ capstone-frontend/capstone-react-app/src/components/Home.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import Twelve4 from "./Twelve4.png";
 import images from "./images";
 import ImageSlider from "./imageSlider";
-import image1 from "./image1.png";
 
 export default function Home() {
     return (
@@ -14,15 +13,11 @@ export default function Home() {
       
       <div className="yelp-review">
         <span className="yelp-review" data-review-id="WDjunj7EP_X9z18z_4cg_w" 
-        data-hostname="www.yelp.com">Read  
-        <a href="https://www.yelp.com/user_details?userid=3kffGUf3oz__Sc8MZgQWlA" 
-        rel="nofollow noopener"> Luana O.</a>'s 
-        <a href="https://www.yelp.com/biz/don-julios-mexican-kitchen-and-tequila-bar-lake-mary?hrid=WDjunj7EP_X9z18z_4cg_w" 
-        rel="nofollow noopener"> review</a> of 
-        <a href="https://www.yelp.com/biz/Hm3k86w9CHNAR7dR1oJwWA"
-        rel="nofollow noopener"> Don Julios Mexican Kitchen and Tequila Bar</a> on 
-        <a href="https://www.yelp.com" rel="nofollow noopener"
-        > Yelp</a><script src="https://www.yelp.com/embed/widgets.js" 
+        data-hostname="www.yelp.com">Read <a href="https://www.yelp.com/user_details?userid=3kffGUf3oz__Sc8MZgQWlA" 
+        rel="nofollow noopener">Luana O.</a>'s <a href="https://www.yelp.com/biz/don-julios-mexican-kitchen-and-tequila-bar-lake-mary?hrid=WDjunj7EP_X9z18z_4cg_w" 
+        rel="nofollow noopener">review</a> of <a href="https://www.yelp.com/biz/Hm3k86w9CHNAR7dR1oJwWA"
+        rel="nofollow noopener">Don Julios Mexican Kitchen and Tequila Bar </a>on <a href="https://www.yelp.com" rel="nofollow noopener"
+        >Yelp</a><script src="https://www.yelp.com/embed/widgets.js" 
         type="text/javascript" async></script></span>
         
         </div>
--- capstone-frontend/capstone-react-app/src/index.js
+++ capstone-frontend/capstone-react-app/src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./style.css";
+import "./home.css";
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
