74bde70de6fa6dd4fa981a9d1c74d748983f37cb
Gregory Gennaro <gennarogreg@gmail.com>
2022-06-16 13:53:02 -0500

    fixed navigation

--- capstone-frontend/capstone-react-app/src/components/NavBar.js
+++ capstone-frontend/capstone-react-app/src/components/NavBar.js
@@ -1,15 +1,17 @@
-import { Link } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
+import Twelve5 from "./Twelve5.png";
+
 
 export default function NavBar() {
   return (
-<div>
+    <div>
       <h1
         style={{
           textAlign: "center",
         }}
-        >
-        12 Eyes Community Theater
-        </h1>
+      >
+        <img className="logo" src={Twelve5} style={{ padding: "0rem", width: 300, height: 300 }} />
+      </h1>
       <nav
         style={{
           borderBottom: "solid 1px",
@@ -17,12 +19,13 @@ export default function NavBar() {
           paddingRight: "1rem",
           textAlign: "center",
         }}
-        >
+      >
         <Link to="/home"><button className="btn btn-outline-success">Home</button></Link> {" "}
         <Link to="/login"><button className="btn btn-outline-success">Log In</button></Link> {" "} {/* Will be Log Out when logged in */}
         <Link to="/signup"><button className="btn btn-outline-success">Sign Up</button></Link> {" "}
-        <Link to="/schedule"><button className="btn btn-outline-success">Audition Schedule</button></Link> {/*Admin Only eventually*/} 
+        <Link to="/schedule"><button className="btn btn-outline-success">Audition Schedule</button></Link> {/*Admin Only eventually*/}
       </nav>
+      <Outlet />
     </div>
   );
 }
