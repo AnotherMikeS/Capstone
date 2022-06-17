0f30d1de18d52ddc52f247d2c4e39dd4c294cd3f
Shelley Nixon <shellnix4@gmail.com>
2022-06-17 17:06:04 -0400

    working on my account and edit pages

--- capstone-frontend/capstone-react-app/src/App.js
+++ capstone-frontend/capstone-react-app/src/App.js
@@ -7,24 +7,25 @@ import Edit from "./components/Edit";
 //import './UserForms.css';
 import NavBar from "./components/NavBar";
 import UserForms from "./components/UserForms";
+import MyAccount from "./components/MyAccount";
 
 export default function App() {
-    const [userStatus, setUserStatus] = useState({username: null, userId: 0});
+    const [userStatus, setUserStatus] = useState({ username: null, userId: 0 });
 
-    const login = (username, userId) =>{
-        setUserStatus({...userStatus, username: username, userId: userId})
+    const login = (username, userId) => {
+        setUserStatus({ ...userStatus, username: username, userId: userId })
     };
 
     const logout = () => {
         // "token" must match the name used in "/Login" route
         localStorage.removeItem("token");
         localStorage.removeItem("id");
-        setUserStatus((userStatus) => ({ ...userStatus, username: null, userId: 0}));
+        setUserStatus((userStatus) => ({ ...userStatus, username: null, userId: 0 }));
     };
 
     return (
         <BrowserRouter>
-            <NavBar userStatus={userStatus} logout={logout}/>
+            <NavBar userStatus={userStatus} logout={logout} />
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/home" element={<Home />} />
@@ -32,6 +33,7 @@ export default function App() {
                 <Route path="/signup" element={<SignUp />} />
                 <Route path="/edit" element={<Edit />} />
                 <Route path="/schedule" element={<Schedule />} />
+                <Route path="/myaccount" element={<MyAccount />} />
 
                 {/* <Route path="/login" element={userStatus.user ? (<Navigate to="/" />) : (<LogIn userStatus={userStatus} test={3} />)}/> */}
             </Routes>
--- capstone-frontend/capstone-react-app/src/components/MyAccount.js
+++ capstone-frontend/capstone-react-app/src/components/MyAccount.js
@@ -8,10 +8,40 @@ import React, { useState, useEffect } from 'react';
 // edit button which takes you to edit page
 
 export default function MyAccount() {
+    // setState
+    // user
+    const [user, setUser] = ([]);
+    const [persons, setPersons] = useState([]);
+    const [username, setUsername] = useState("");
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
 
-// get useEffect
+    useEffect(() => {
+        // get a list of all people
+        fetch("http://localhost:8080/api/theater/person")
+            .then(resp => resp.json())
+            .then(data => {
+                setPersons(data);
+            })
+    }, []);
 
-// display user's account info
+    console.log("all persons: ", persons);
+    var userId = localStorage.getItem("id");
+    console.log("user id: ", userId);
+    var personId;
+
+    // loop through all people until we find the app_user_id that matches the global state
+    for (var i = 0; i < persons.length; i++) {
+
+        // take that result and get the person_id
+        if (persons[i].appUserId === userId) {
+            personId = (persons[i].personId)
+        }
+        console.log("person id: ", personId);
+
+    }
+
+    // display user's account info
 
 
 
