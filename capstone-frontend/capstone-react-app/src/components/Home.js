ee96b9586c18fdc11f3e33e2fe8ff9647303c85b
Shelley Nixon <shellnix4@gmail.com>
2022-06-20 12:39:43 -0400

    working on map on home page

--- capstone-frontend/capstone-react-app/src/components/Home.js
+++ capstone-frontend/capstone-react-app/src/components/Home.js
@@ -18,6 +18,11 @@ export default function Home() {
         <div className="calloutBox-container">Auditioning Now
         </div>
       </div>
+
+      <div className="map">
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2988.2043296050037!2d-81.6825912845413!3d41.49984669697875!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8830fa7d31bc7e1d%3A0xd4fedf1ad239019!2sPlayhouse%20Square!5e0!3m2!1sen!2sus!4v1655742959498!5m2!1sen!2sus" width="600" height="450" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+      </div>
+
     </main>
 
   );
