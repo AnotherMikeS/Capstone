b78845a53f4f98f9dfd71fcd465e993fee5c32fc
Shelley Nixon <shellnix4@gmail.com>
2022-06-21 15:27:32 -0400

    docker and deploy changes

--- capstone-backend/src/main/java/learn/capstone/controllers/PersonController.java
+++ capstone-backend/src/main/java/learn/capstone/controllers/PersonController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.*;
 
 
 @RestController
-@CrossOrigin(origins = {"http://localhost:3000"})
 @RequestMapping("/api/theater/person")
 
 public class PersonController {
